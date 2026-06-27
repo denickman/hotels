ed0ba3b46ad32009af6db44d155858885482c752
Dennis Yaremenko <yaremdennis@gmail.com>
2026-06-27 18:27:13 +0300

    add auth

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from numpy.ma.core import squeeze
 
 # С dtype={"id": str} pandas прочитает id как текст (строка):
 df = pandas.read_csv('hotels.csv', dtype={"id": str})
+df_cards = pandas.read_csv('cards.csv', dtype=str).to_dict(orient='records')
+df_cards_security = pandas.read_csv('card_security.csv', dtype=str)
 
 
-
+#  dtype={"id": str} - читать столбец 'id' как текст, не как число.
 # df.loc[] возвращает результат в виде таблицы (Series), даже если это одна ячейка:
-# .squeeze() извлекает значение из таблицы и превращает его в обычное значение.
+# .squeeze() извлекает значение из таблицы и превращает его в обычное значение /  обычную строку:.
 
 class Hotel:
     def __init__(self, hotel_id):
@@ -16,6 +18,9 @@ class Hotel:
 
     def book_hotel(self):
         '''book a hotel by changing availability to no'''
+
+        # Находит строку где id == self.id
+        # Присваивает значение 'no' в столбец 'available'
         df.loc[df['id'] == self.id, 'available'] = 'no'
         df.to_csv('hotels.csv', index=False)  # <- нужно передать имя файла
 
@@ -45,7 +50,42 @@ class ReservationTicket:
         return content  # <- добавь return
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expDate, holder, cvv):
+        for card in df_cards:
+            if (card['number'] == self.number and
+                    card['expiration'] == expDate and
+                    card['holder'] == holder and
+                    card['cvc'] == cvv):
+                return True
+        return False
+
+
+class SecureCreditCard(CreditCard):
+
+    def authenticate(self, given_password):
+        row = df_cards_security.loc[df_cards_security['number'] == self.number]
+
+        if row.empty:  # если карта не найдена
+            return False
+
+        password = row['password'].values[0]  # извлеки первое значение
+
+        if password == given_password:
+            return True
+        else:
+            return False
+
+
+
+
+
+
 print(df)
+
 
 while True:
     hotel_id = input("Enter hotel id: ")
@@ -57,10 +97,22 @@ while True:
         hotel = Hotel(hotel_id)
 
         if hotel.available():
-            hotel.book_hotel()
 
-            name = input('Enter your name: ')
-            reservation_ticket = ReservationTicket(name, hotel)
-            print(reservation_ticket.generate())
+            # card_number = input("Enter card number: ")
+
+            credit_card = SecureCreditCard(number='1234')
+
+            if credit_card.validate(expDate = '12/26', holder = 'JOHN SMITH', cvv='123'):
+                if credit_card.authenticate(given_password = 'mypass'):
+
+                    hotel.book_hotel()
+
+                    name = input('Enter your name: ')
+                    reservation_ticket = ReservationTicket(name, hotel)
+                    print(reservation_ticket.generate())
+                else:
+                    print("Credit card auth failed")
+            else:
+                print("there was a problem with your method")
         else:
             print("hotel is not free")
